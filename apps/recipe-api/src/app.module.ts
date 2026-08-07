b3bac1279c695e1dd63de726a2d1d515284e2b93
Adam Woźniewski <adam.baaka@gmail.com>
2026-08-08 00:35:16 +0200

    husky test

--- apps/recipe-api/src/app.module.ts
+++ apps/recipe-api/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
-// import { UserModule } from './auth/user/user.module';
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -18,7 +17,6 @@ import { RecipeModule } from "./recipe/recipe.module";
       envFilePath: [`.env.${process.env.NODE_ENV}`],
       validationSchema: envValidationConfig,
     }),
-    // UserModule,
     AuthModule,
   ],
   controllers: [AppController],
